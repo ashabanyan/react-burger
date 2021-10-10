4fc531dcc4f186f80479969517bb9ab4caf7688a
Александр Шабанян <ashabanyan.dev@gmail.com>
2021-10-10 20:16:18 +0300

    Sprint-1_first-deadline_second-iteration_fix

--- src/components/ingredient-item/ingredient-item.jsx
+++ src/components/ingredient-item/ingredient-item.jsx
@@ -3,18 +3,18 @@ import styles from '../ingredient-item/ingredient-item.module.css'
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 
-const IngredientItem = () => {
+const IngredientItem = (props) => {
 
   return (
     <li className={`${styles.ingredient_item} mb-10`}>
-      <img className={styles.image} src={this.props.ingredient.image} alt={this.props.ingredient.name}/>
+      <img className={styles.image} src={props.ingredient.image} alt={props.ingredient.name}/>
 
       <div className={styles.price}>
-        <p className={`${styles.price_text} text text_type_main-small mr-1`}>{this.props.ingredient.price}</p>
+        <p className={`${styles.price_text} text text_type_main-small mr-1`}>{props.ingredient.price}</p>
         <CurrencyIcon type="primary" />
       </div>
 
-      <p className={`${styles.name_text} text text_type_main-default mt-1`}>{this.props.ingredient.name}</p>
+      <p className={`${styles.name_text} text text_type_main-default mt-1`}>{props.ingredient.name}</p>
 
       <Counter count={1} size="default" />
     </li>
